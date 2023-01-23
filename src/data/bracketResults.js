c33ad0334f2287388fba9727896c64b33b3e88b2
hansonjw <hansonjw@gmail.com>
2023-01-23 09:33:17 -0600

    Added bracket results...

--- src/data/bracketResults.js
+++ src/data/bracketResults.js
@@ -1,9 +1,9 @@
 const bracketResults = {
         username:"NFL Results",
-        AD1:"NFL",
-        AD2:"NFL",
-        ND1:"NFL",
-        ND2:"NFL",
+        AD1:"KC",
+        AD2:"CIN",
+        ND1:"PHI",
+        ND2:"SF",
         AC1:"NFL",
         NC1:"NFL",
         SB: "NFL"
